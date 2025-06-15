83cd761c8b91b1d87a3d09ba24bd61ddd1a67413
Vadim Kiniabaev <thekinv21@gmail.com>
2025-06-15 15:05:38 +0300

    feat:[master][Vadim]: Prettier code düzeni eklendi

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
+
 import { Providers } from './providers/index'
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<Providers />
-	</StrictMode>
+  <StrictMode>
+    <Providers />
+  </StrictMode>
 )
