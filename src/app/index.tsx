a5a06d1812730f1252095c4cb17cf16e698992a8
Vadim Kiniabaev <thekinv21@gmail.com>
2025-06-15 15:23:26 +0300

    feat:[master][Vadim]: React-Scan eklendi fazla re-renderları yakalamak ve düzeltmek için kullanılan kütüphane

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { scan } from 'react-scan'
 
 import './index.css'
 
 import { Providers } from './providers/index'
+
+scan({
+  enabled: true
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
