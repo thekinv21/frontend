d6e4cbe653fb76d35921422790d68b2389146693
Vadim Kiniabaev <thekinv21@gmail.com>
2025-06-15 15:00:29 +0300

    feat:[master][Vadim]: Dosya altyapısı oluşturuldu ve bütün dosya yapısına index.ts Public api dosyası eklendi

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Providers } from './providers'
+import { Providers } from './providers/index'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
