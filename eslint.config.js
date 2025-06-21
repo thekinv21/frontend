e0c446e9c609ae25fb8b4bc56b89e57fb89e17e6
Vadim Kiniabaev <thekinv21@gmail.com>
2025-06-21 11:28:22 +0300

    feat: Eslint Config
    1) Typescript Any tipi kullanılmaması kuralı eklendi
    
    2)  Boş obje  kullanılmaması kuralı eklendi
    
    3)  Kullanılmayan değişkenler için  kullanılmama kuralı eklendi

--- eslint.config.js
+++ eslint.config.js
@@ -18,11 +18,31 @@ export default tseslint.config(
       'react-refresh': reactRefresh
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true }
-      ]
+      ],
+
+      /**
+       * Any tipini kullanmaya izin verme
+       * @see https://typescript-eslint.io/rules/no-explicit-any/
+       * @see https://eslint.org/docs/latest/rules/no-explicit-any
+       */
+      '@typescript-eslint/no-explicit-any': 'error',
+
+      /**
+       * Kullanılmayan değişkenler için hata ver
+       * @see https://typescript-eslint.io/rules/no-unused-vars/
+       * @see https://eslint.org/docs/latest/rules/no-unused-vars
+       */
+      '@typescript-eslint/no-unused-vars': 'error',
+
+      /**
+       * Boş object ise hata ver
+       * @see https://typescript-eslint.io/rules/no-empty-object-type/
+       * @see https://eslint.org/docs/latest/rules/no-empty-object-type
+       */
+      '@typescript-eslint/no-empty-object-type': 'error'
     }
   }
 )
