9c130b8e34c67f9f0fdb4901add28ed0152331f9
Vadim Kiniabaev <thekinv21@gmail.com>
2025-07-02 00:10:53 +0300

    fix: [master][Vadim Kiniabaev]:  Loader eklendi ve custom css eklendi loader için

--- src/shared/ui/Loader.tsx
+++ src/shared/ui/Loader.tsx
@@ -1,13 +1,35 @@
+import { LoaderCircleIcon } from 'lucide-react'
+
 import { cn } from '../lib'
 
-export const Loader = ({ className }: { className?: string }) => (
-  <div
-    className={cn(
-      `inline-block size-7 animate-spin rounded-full border-4 border-solid
-      border-current border-r-transparent`,
-      className
-    )}
-    role='status'>
-    <span className='sr-only'>Loading...</span>
-  </div>
-)
+export function Loader() {
+  return (
+    <div
+      className='fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center
+        backdrop-blur-md'>
+      <LoaderCircleIcon
+        className='animate-custom-spin text-primary transition-colors ease-linear'
+        size={150}
+        strokeWidth={1}
+      />
+    </div>
+  )
+}
+
+type SmallLoaderProps = {
+  className?: string
+}
+
+export function SmallLoader({ className }: SmallLoaderProps) {
+  return (
+    <div className='flex h-full w-full items-center justify-center'>
+      <LoaderCircleIcon
+        className={cn(
+          'animate-custom-spin text-secondary-primary transition-colors ease-linear',
+          className
+        )}
+        strokeWidth={1}
+      />
+    </div>
+  )
+}
